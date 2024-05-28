169121bb221c29b89e892d36d8768c6c87e7dd37
gahlii <gahlo532@gmail.com>
2024-05-28 17:15:18 +0300

    initial

---
+++ app/api/api-hooks.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import { getNormalizedGamesDataByCategory, isResponseOk } from './api-utils'
+
+export const useGetDataByCategory = (endpoint, category) => {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getNormalizedGamesDataByCategory(endpoint, category)
+      setData(data)
+    }
+    fetchData()
+  }, [])
+  if (isResponseOk) {
+    return data
+  }
+  else{
+    console.log('Ошибка при получении игр по категории')
+  }
+}
